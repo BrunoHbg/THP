caae70167a22bb49dd6e4f4b15ff9a59b5d06fcf
BrunoHbg <71728923+BrunoHbg@users.noreply.github.com>
2020-09-29 20:23:26 +0200

    Update exo_02.rb

--- exo_02.rb
+++ exo_02.rb
@@ -1,2 +1,4 @@
 puts "Bonjour, monde !"
 puts "Et avec une voix sexy, ça donne : bonjour, monde !"
+
+#Lorsque l'on utilise la fonction put, le texte va à la ligne contrairement à la fonction print.
